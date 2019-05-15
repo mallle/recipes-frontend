9f979d30797efde2bf5093e3f14b06a7ce1f7222
Malene Klit <m.klit@linksderisar.com>
2019-05-15 15:07:15 +0200

    add content to recipe-page

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -7,23 +7,16 @@
         </div>
         <div class="card-content">
             <div class="media">
-                <div class="media-left">
-                    <figure class="image is-48x48">
-                        <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                    </figure>
-                </div>
                 <div class="media-content">
                     <router-link :to="`/recipe/${recipe.id}`" tag="p" class="title is-4">{{ recipe.name }}</router-link >
-                    <p class="subtitle is-6">@johnsmith</p>
                 </div>
             </div>
 
+
             <div class="content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                <a href="#">#css</a> <a href="#">#responsive</a>
-                <br>
-                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                <b-taglist>
+                    <b-tag v-for="(tag, index) in recipe.tags" :key="index" size="is-medium">{{ tag.name }}</b-tag>
+                </b-taglist>
             </div>
         </div>
     </div>
--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -1,10 +1,89 @@
 <template>
-    <div>
-        <p>test</p>
+    <div class="container">
+        <figure class="image is-2by1">
+            <img :src="recipe.image">
+        </figure>
+
+        <div class="section">
+            <nav class="level">
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Preparation time</p>
+                        <p class="title">{{recipe.preparationtime}}</p>
+                    </div>
+                </div>
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Resttime</p>
+                        <p class="title">{{ recipe.resttime }}</p>
+                    </div>
+                </div>
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Baking time</p>
+                        <p class="title">{{ recipe.bakingtime }}</p>
+                    </div>
+                </div>
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Effort</p>
+                        <p class="title">{{ recipe.effort }}</p>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class="section">
+            <div class="columns">
+                <div class="column is-5">
+
+                        <b-icon
+                                pack="fas"
+                                icon="plus"
+                                size="is-small">
+                        </b-icon>
+                        <span class="number-of-persons">{{recipe.persons}}</span>
+                        <b-icon
+                                pack="fas"
+                                icon="minus"
+                                size="is-small">
+                        </b-icon>
+
+                    <table class="table is-narrow">
+                        <tbody>
+                            <tr v-for="(ingredient, index) in recipe.ingredients" :key="index">
+                                <td class="has-text-right">{{ingredient.amount }}<td>
+                                <td>{{ingredient.type }} {{ingredient.name }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="block">
+                        <b-taglist>
+                            <b-tag v-for="(tag, index) in recipe.tags" :key="index" type="is-info" size="is-medium">{{ tag.name }}</b-tag>
+                        </b-taglist>
+                    </div>
+
+                </div>
+                <div class="column has-text-left">
+                    <div class="content" v-for="step in recipe.descriptions" :key="step.id">
+                        <div class="tags are-medium">
+                            <span class="tag">{{step.descriptionnumber}}</span>
+
+                        </div><hr>
+
+                        <p class="description">{{step.description}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <pre>{{recipe}}</pre>
     </div>
 </template>
 
 <script>
+
+    import axios from 'axios'
+
     export default {
         data(){
             return{
@@ -13,7 +92,7 @@
         },
         mounted() {
             axios
-                .get(`https://www.recipes.test/api/recipes/${this.id}`)
+                .get(`https://www.recipes.test/api/recipes/${this.$route.params.id}`)
                 .then(r => (this.recipe = r.data.data));
 
         },
@@ -21,5 +100,19 @@
 </script>
 
 <style scoped>
+    .number-of-persons{
+        margin: 0 10px 0 10px;
+        font-size: 1.5rem;
+    }
+    .table td, .table th {
+        border-color: white;
+    }
 
+    hr{
+        margin: -2.5rem 8rem 1rem 51px;
+    }
+
+    .description{
+        padding-left: 3.3rem;
+    }
 </style>
